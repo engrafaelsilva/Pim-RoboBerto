f0bd88622c85d9d3e2d8219efce557eef60bda3b
Douglas <dengratulis@gmail.com>
2025-10-30 10:39:50 -0300

    Inserindo algumas validações

--- IA-RoboBerto/DTOs/ChamadoInsertDTO.cs
+++ IA-RoboBerto/DTOs/ChamadoInsertDTO.cs
@@ -1,14 +1,23 @@
 ﻿using IA_RoboBerto.Modelos;
 using IA_RoboBerto.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace IA_RoboBerto.DTOs
 {
     public class ChamadoInsertDTO
     {
-     //   public Guid Id { get; set; }
+        //   public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Erro: a categoria do chamado é obrigatório.")]
         public CategoriaNomeDTO Categoria { get; set; }
+
+        [Required(ErrorMessage = "Erro: a prioridade do chamado é obrigatório.")]
         public EPrioridade Prioridade { get; set; }
+
+        [Required(ErrorMessage = "Erro: o título do chamado é obrigatório.")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Erro: a descrição do chamado é obrigatório.")]
         public string Descricao { get; set; }
         public IList<MensagemDTO>? Mensagens { get; } = new List<MensagemDTO>();
 
--- IA-RoboBerto/DTOs/MensagemInsertDTO.cs
+++ IA-RoboBerto/DTOs/MensagemInsertDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace IA_RoboBerto.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IA_RoboBerto.DTOs
 {
     public class MensagemInsertDTO
     {
-       // public Guid Id { get; set; }
+        // public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Erro: Campo é requerido")]
         public string Texto { get; set; }
 
         public MensagemInsertDTO()
